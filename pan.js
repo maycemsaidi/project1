3682a517174040b8b123b6957500f21e72b26ee5
maycemsaidi <141725047+maycemsaidi@users.noreply.github.com>
2023-08-25 09:18:56 +0100

    Add files via upload

---
+++ pan.js
@@ -0,0 +1,28 @@
+// document.addEventListener('DOMContentLoaded', function () {
+//     const cartList = document.getElementById('cart-list');
+//     const cartTotal = document.getElementById('cart-total');
+//     const commanderBtn = document.getElementById('commander-btn');
+    
+
+//     const selectedProducts = JSON.parse(localStorage.getItem('selectedProducts')) || [];
+
+//     let total = 0;
+//     let product.quantity=1
+//     selectedProducts.forEach(product => {
+//         const listItem = document.createElement('li');
+//         listItem.textContent = $('selectedProducts');
+//         cartList.appendChild(listItem);
+
+//         // Calcul du prix total pour chaque produit
+//         if(product.quantity>1 && product.price!=0){
+//         const productTotal = product.price * product.quantity;
+//         total += productTotal;}
+//     });
+
+//     cartTotal.textContent = total.toFixed(3) + ' TND'; // Affichez le total avec deux décimales
+
+//     commanderBtn.addEventListener('click', () => {
+//         alert('Commande passée !');
+//     });
+// });
+
